a79727c1fa4569450a10e831159d7f66ea6f7098
Ashish Singh <singhashish82103@gmail.com>
2024-05-21 10:14:32 +0530

    Changes

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -3,12 +3,13 @@ import Ashish from "./ashish"
 
 
 function App() {
-  
+  const userName = 'singhashish82103'
 
   return (
     <>
+    
     <Ashish/>
-    <h1>Ashish React</h1>
+    <h1>Ashish React {userName}</h1>
     <p>Test paragraph</p>
     </>
     
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -2,9 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return(
+    <div>
+      <h1>Custum App! </h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = "singhashish82103@gmail.com";
+const reactElement = React.createElement(
+  'a',
+  {href: "https://google.com", target: '_blank'},
+  'Click me to visit google',
+  anotherUser
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    reactElement
+    // <App/>
+    
+
+  
 )
