05d374799d0ee9bf3bfd4816aef94dd4e5264661
Ashish Singh <singhashish82103@gmail.com>
2024-05-23 09:39:04 +0530

    Props

--- 04_tailwindProps/src/App.jsx
+++ 04_tailwindProps/src/App.jsx
@@ -6,20 +6,25 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0);
-  
+
   let myObj = {
     userName: 'ashish',
-    age: 27
+    age: 27,
+    isLoggedIn: false,
   }
+
+ let myArr = [1, 2, 3, 4];
+
+ 
+ 
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card channel='singhashish' someObj = myObj={}/>
-      <Card/>
 
-  
-   
+      <Card username='singhashish' someObj = {myObj} otherObj = {myArr} btnText ="Click-me"/>
+      <Card username= "ashish" btnText ="Visit-me"/>
+
     </>
   )
 }
